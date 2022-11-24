47cda693e7e69b6ea9526c426aa29adc54b2284d
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-11-24 02:04:39 +0100

    setup user signup

--- fedvosa/fedvosa/core/urls.py
+++ fedvosa/fedvosa/core/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup', views.signup, name='signup'),
+    path('faq', views.faq, name='faq'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('pay', views.pay, name='pay'),
 ]
--- fedvosa/fedvosa/core/views.py
+++ fedvosa/fedvosa/core/views.py
@@ -4,3 +4,59 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signup(request):
+    pin_lists = ['111a', '222b', '333c', '444d', '555e', '666f']
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']        
+        pin = request.POST['pin']
+        # pin = request.POST['pin']
+
+        if pin in pin_lists:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            # elif pin not in pin_lists:
+            #     messages.info(request, 'Incorrect Pin')
+            #     return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
+                #remove the entered pin from the pin_lists
+                pin_lists.remove(pin)
+        else:
+            messages.info(request, 'Invalid PIN')
+            return redirect('signup')
+        
+    else:
+        return render(request, 'signup.html')
+
+
+# Additional contents
+def faq(request):
+    return render(request, 'faq.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def pay(request):
+    return render(request, 'pay.html')
