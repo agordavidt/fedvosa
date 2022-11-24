dc05e5a8ef1c32fa3527bfeddf73cab758716d80
agordavidt <98212267+agordavidt@users.noreply.github.com>
2022-11-24 03:23:13 +0100

    login setup

--- fedvosa/fedvosa/core/urls.py
+++ fedvosa/fedvosa/core/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
     path('faq', views.faq, name='faq'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
--- fedvosa/fedvosa/core/views.py
+++ fedvosa/fedvosa/core/views.py
@@ -52,6 +52,24 @@ def signup(request):
         return render(request, 'signup.html')
 
 
+def signin(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
+    else:
+        return render(request, 'signin.html')
+
 # Additional contents
 def faq(request):
     return render(request, 'faq.html')
